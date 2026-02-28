75b88b51eae5b0b9015b7851b951d4631611c1dd
kkbhong71 <139850574+kkbhong71@users.noreply.github.com>
2026-02-28 23:01:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def predict():
     Returns:
         JSON: 예측 결과
         {
-            "latest_round": 1212,
+            "latest_round": 1213,
             "window": 50,
             "hot_pool_size": 32,
             "cold_pool_size": 13,
